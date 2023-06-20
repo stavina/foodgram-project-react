c8b1a35dd053c60376ac8ad0818386d4a32c4a89
stavina <104823170+stavina@users.noreply.github.com>
2023-06-20 09:44:29 +0300

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Follow, User
 
 admin.site.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+  list_display = ('id', 'username', 'email', 'first_name', 'last_name',)
+  search_fields = ('username', 'email',) 
+  list_filter = ('email', 'first_name',) 
+  empty_value_display = '-пусто-'
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author',)
+    search_fields = ('author', 'user',)
+    list_filter = ('author',)
