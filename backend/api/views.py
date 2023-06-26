7bd06ed86aef8a76494541c47d4f8be7ed0e02b5
stavina <104823170+stavina@users.noreply.github.com>
2023-06-26 12:12:10 +0300

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.db.models import F, Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
